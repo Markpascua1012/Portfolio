9c2b78a17b05153fdf224d2fdd4eb3bcb6523913
Mark Pascua <markpascua1012@gmail.com>
2019-09-09 13:54:25 -0700

    Updates

---
+++ precache-manifest.0952cf9b353615fbcafb1dce905fb5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11577a06d8d2332b750c6b3c5fe011f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d79ad23e2d15eb6125d0",
+    "url": "/Portfolio/static/css/main.b1767454.chunk.css"
+  },
+  {
+    "revision": "0f19edae200c0d9bccaa",
+    "url": "/Portfolio/static/js/2.8dc25040.chunk.js"
+  },
+  {
+    "revision": "d79ad23e2d15eb6125d0",
+    "url": "/Portfolio/static/js/main.92962a26.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "4857fe6f15336bf7e1130320161d39b1",
+    "url": "/Portfolio/static/media/Jaden.4857fe6f.ttf"
+  }
+]);
